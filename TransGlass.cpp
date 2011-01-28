b10619f075bb594ec6b5a8412eb6da3cb74b0da6
Cheng Jiang <cheney.jiang@gmail.com>
2011-01-28 21:50:17 +0800

    Init project repo.

---
+++ TransGlass.cpp
@@ -0,0 +1,85 @@
+
+// TransGlass.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "TransGlass.h"
+#include "TransGlassDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CTransGlassApp
+
+BEGIN_MESSAGE_MAP(CTransGlassApp, CWinApp)
+    ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CTransGlassApp construction
+
+CTransGlassApp::CTransGlassApp()
+{
+    // TODO: add construction code here,
+    // Place all significant initialization in InitInstance
+}
+
+
+// The one and only CTransGlassApp object
+
+CTransGlassApp theApp;
+
+
+// CTransGlassApp initialization
+
+BOOL CTransGlassApp::InitInstance()
+{
+    // InitCommonControlsEx() is required on Windows XP if an application
+    // manifest specifies use of ComCtl32.dll version 6 or later to enable
+    // visual styles.  Otherwise, any window creation will fail.
+    INITCOMMONCONTROLSEX InitCtrls;
+    InitCtrls.dwSize = sizeof(InitCtrls);
+    // Set this to include all the common control classes you want to use
+    // in your application.
+    InitCtrls.dwICC = ICC_WIN95_CLASSES;
+    InitCommonControlsEx(&InitCtrls);
+
+    CWinApp::InitInstance();
+
+
+    // Create the shell manager, in case the dialog contains
+    // any shell tree view or shell list view controls.
+    CShellManager *pShellManager = new CShellManager;
+
+    // Standard initialization
+    // If you are not using these features and wish to reduce the size
+    // of your final executable, you should remove from the following
+    // the specific initialization routines you do not need
+    // Change the registry key under which our settings are stored
+
+    CTransGlassDlg dlg;
+    m_pMainWnd = &dlg;
+    INT_PTR nResponse = dlg.DoModal();
+    if (nResponse == IDOK)
+    {
+        // TODO: Place code here to handle when the dialog is
+        //  dismissed with OK
+    }
+    else if (nResponse == IDCANCEL)
+    {
+        // TODO: Place code here to handle when the dialog is
+        //  dismissed with Cancel
+    }
+
+    // Delete the shell manager created above.
+    if (pShellManager != NULL)
+    {
+        delete pShellManager;
+    }
+
+    // Since the dialog has been closed, return FALSE so that we exit the
+    //  application, rather than start the application's message pump.
+    return FALSE;
+}
